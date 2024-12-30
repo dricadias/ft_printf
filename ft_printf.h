7ecd9f07bb314307463ad0455d85a2e73b3f68ba
Andrielly Dias <andrielly.12dias.ad@gmail.com>
2024-12-30 23:09:15 +0000

    ft_printf header
    ft_printf header

---
+++ ft_printf.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adias-do <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/18 14:25:22 by adias-do          #+#    #+#             */
+/*   Updated: 2024/11/22 18:51:02 by adias-do         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <unistd.h>
+# include <stdarg.h>
+
+int	ft_printf(const char *format, ...);
+int	ft_putchar(char c);
+int	ft_putstr(char *s);
+int	ft_putnbr(int nb);
+int	ft_putunbr(unsigned int nb);
+int	ft_hex(unsigned int dec, char t);
+int	ft_pointer(void *ptr);
+
+#endif
